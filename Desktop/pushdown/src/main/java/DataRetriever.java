113fb8e5f27cac668f6953d192eb34cdd2bdc7e5
fenohasina08 <fenohasinaherimamy@gmail.com>
2026-02-18 21:45:26 +0300

    feat: daataretriver

---
+++ Desktop/pushdown/src/main/java/DataRetriever.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+public class DataRetriever {
+    private final String url = "jdbc:postgresql://localhost:5432/pushdown";
+    private final String user = "postgres";
+    private final String password = "fenohasina3123";
+
+    public List<InvoiceTotal> findInvoiceTotals () {
+        List<InvoiceTotal> result = new ArrayList<>();
+
+    }
+}
